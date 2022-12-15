82f111c663a5dc6cbac944811a152fbc20920fae
Ben Moss <ben@mossity.com>
2022-12-15 13:02:22 -0500

    Monitor watches for completions
    - Simplifies the relationship between the monitor and the scheduler
    - Fixes a bug with the previous "scanKnownJobs" implementation where it
      found jobs that didn't match the configured tags
    - Adds the tag that we acquired the job through as a label so that we
      can correlate it after a restart

--- monitor/monitor_test.go
+++ monitor/monitor_test.go
@@ -5,8 +5,12 @@ import (
 	"os"
 	"testing"
 
+	"github.com/buildkite/agent-stack-k8s/api"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
 )
 
@@ -20,4 +24,42 @@ func TestInvalidOrg(t *testing.T) {
 	require.NoError(t, err)
 	job := <-m.Scheduled()
 	require.ErrorContains(t, job.Err, "invalid organization")
+}
+
+func TestSynchronize(t *testing.T) {
+	tag := "some-tag=yep"
+	ctx := context.Background()
+	jobs := []runtime.Object{
+		&batchv1.Job{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "not-our-job",
+			},
+		},
+		&batchv1.Job{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "different-tag",
+				Labels: map[string]string{
+					api.TagLabel:  "something-else",
+					api.UUIDLabel: "1",
+				},
+			},
+		},
+		&batchv1.Job{
+			ObjectMeta: v1.ObjectMeta{
+				Name: "matching-tag",
+				Labels: map[string]string{
+					api.TagLabel:  TagToLabel(tag),
+					api.UUIDLabel: "2",
+				},
+			},
+		},
+	}
+	client := fake.NewSimpleClientset(jobs...)
+	m, err := New(ctx, zap.Must(zap.NewDevelopment()), client, Config{
+		Org:  "foo",
+		Tags: []string{tag},
+	})
+	require.NoError(t, err)
+	require.Equal(t, 1, m.knownBuilds.Len())
+
 }
