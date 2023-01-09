a30f3e58c90a0017fd1b97adac346d5937d9759a
Ben Moss <ben@mossity.com>
2023-01-09 11:56:35 -0500

    Move linter to a subcommand

--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"log"
-
 	"github.com/buildkite/agent-stack-k8s/cmd/controller"
-	"k8s.io/client-go/kubernetes"
-
-	restconfig "sigs.k8s.io/controller-runtime/pkg/client/config"
-	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
 func main() {
-	ctx := signals.SetupSignalHandler()
-
-	clientConfig := restconfig.GetConfigOrDie()
-	k8sClient, err := kubernetes.NewForConfig(clientConfig)
-	if err != nil {
-		log.Fatalf("failed to create clientset: %v", err)
-	}
-	cfg, err := controller.ParseConfig()
-	if err != nil {
-		log.Fatalf("failed to parse config: %v", err)
-	}
-	controller.Run(ctx, k8sClient, cfg)
+	controller.New().Execute()
 }
