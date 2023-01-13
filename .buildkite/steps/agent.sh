18f003de3e62c1240e0dd0d04b144dc5a11c6a00
Ben Moss <ben@mossity.com>
2023-01-13 15:32:14 -0500

    Build and test against the latest agent image

---
+++ .buildkite/steps/agent.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euxo pipefail
+
+apk add just go jq
+
+docker buildx create --driver kubernetes --use
+just agent ttl.sh/buildkite-agent 1h
+
+image=$(cat dist/metadata.json | jq -r '"\(."image.name")@\(."containerimage.digest")"')
+buildkite-agent meta-data set "image" "${image}"
+
+docker buildx rm
