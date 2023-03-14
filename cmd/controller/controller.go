62dca6319230d5a2267447c9475e6cb7b7904247
Narthana Epa <narthana.epa@gmail.com>
2023-03-14 11:50:52 +1100

    Clean up controller

--- cmd/controller/controller.go
+++ cmd/controller/controller.go
@@ -143,12 +143,15 @@ func Run(ctx context.Context, k8sClient kubernetes.Interface, cfg api.Config) {
 	if err != nil {
 		log.Fatal("failed to create monitor", zap.Error(err))
 	}
+
 	sched := scheduler.New(log.Named("scheduler"), k8sClient, cfg)
 	limiter := scheduler.NewLimiter(log.Named("limiter"), sched, cfg.MaxInFlight)
+
 	informerFactory, err := scheduler.NewInformerFactory(k8sClient, cfg.Tags)
 	if err != nil {
 		log.Fatal("failed to create informer", zap.Error(err))
 	}
+
 	if err := limiter.RegisterInformer(ctx, informerFactory); err != nil {
 		log.Fatal("failed to register limiter informer", zap.Error(err))
 	}
@@ -158,12 +161,12 @@ func Run(ctx context.Context, k8sClient kubernetes.Interface, cfg api.Config) {
 		log.Fatal("failed to register completions informer", zap.Error(err))
 	}
 
-	imagePullBackoffWatcher := scheduler.NewImagePullBackOffWatcher(
+	imagePullBackOffWatcher := scheduler.NewImagePullBackOffWatcher(
 		log.Named("imagePullBackoffWatcher"),
 		k8sClient,
 		cfg,
 	)
-	if err := imagePullBackoffWatcher.RegisterInformer(ctx, informerFactory); err != nil {
+	if err := imagePullBackOffWatcher.RegisterInformer(ctx, informerFactory); err != nil {
 		log.Fatal("failed to register imagePullBackoffWatcher informer", zap.Error(err))
 	}
 
