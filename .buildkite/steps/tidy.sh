69395dde3e33346748030fb6e3dce47b51f0ce63
Narthana Epa <narthana.epa@gmail.com>
2023-03-29 08:05:46 +1100

    Better emoji

--- .buildkite/steps/tidy.sh
+++ .buildkite/steps/tidy.sh
@@ -5,5 +5,5 @@ set -euf
 echo --- :hammer: Installing tools
 apk add --update-cache --no-progress git just
 
-echo --- :hammer: Checking tidiness of go modules
+echo --- :broom: Checking tidiness of go modules
 just gomod
