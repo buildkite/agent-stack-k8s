e60b1bca6903f3683420dd0342b8f03ab0e34174
Narthana Epa <narthana.epa@gmail.com>
2023-04-17 14:59:59 +1000

    Check code generation in CI

---
+++ .buildkite/steps/check-code-generation.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+set -euf
+
+echo --- :golang: Generating code
+go generate ./...
+
+echo --- :git: Checking generated code matches commit
+# TODO: remove `-G.` once chmod 777 issue is fixed
+if ! git diff -G. --no-ext-diff --exit-code; then
+  echo +++ :x: Generated code was not commited.
+  echo "Run"
+  echo "  go generate ./..."
+  echo "and make a commit."
+
+  exit 1
+fi
