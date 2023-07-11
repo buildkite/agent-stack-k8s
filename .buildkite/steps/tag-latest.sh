9ff7154f89a97303b74a8aedb217bc09203ba41e
Narthana Epa <narthana.epa@gmail.com>
2023-07-11 13:05:05 +1000

    Fix emoji

--- .buildkite/steps/tag-latest.sh
+++ .buildkite/steps/tag-latest.sh
@@ -8,12 +8,12 @@ apk add --update-cache --no-progress crane git
 echo --- :git::docker: determining version and tags
 source .buildkite/steps/repo_info.sh
 
-echo --- :doker: Logging into ghcr.io
+echo --- :docker: Logging into ghcr.io
 crane auth login ghcr.io \
   --username "$REGISTRY_USERNAME" \
   --password "$REGISTRY_PASSWORD"
 
-echo --- :crane: tagging images latest on ghcr.io
+echo --- :docker: tagging images latest on ghcr.io
 crane tag "$controller_image" latest
 crane tag "$agent_image" latest
 crane tag "${helm_image#oci://}" latest
