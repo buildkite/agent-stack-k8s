1b3a2c57156a1afaa30664279b9edf18e667a0e6
Narthana Epa <narthana.epa@gmail.com>
2023-12-06 08:35:49 +1100

    Remove appending args for the cleanup pipeline behaviour

--- .buildkite/steps/tests.sh
+++ .buildkite/steps/tests.sh
@@ -5,24 +5,14 @@ set -eufo pipefail
 echo "--- Installing gotestsum :golang::test_tube:"
 go install gotest.tools/gotestsum
 
-echo '+++ Running integration tests :test_tube:'
+echo "+++ Running integration tests :test_tube:"
 package="github.com/buildkite/agent-stack-k8s/v2/internal/integration_test"
 branch="${BUILDKITE_BRANCH:-main}"
-IMAGE=$(buildkite-agent meta-data get "agent-image")
+IMAGE=$(buildkite-agent meta-data get agent-image)
 export IMAGE
 
-args=(
-  --junitfile "junit-${BUILDKITE_JOB_ID}.xml"
-  -- \
+gotestsum --junitfile "junit-${BUILDKITE_JOB_ID}.xml" -- \
   -count=1 \
   -failfast \
   -ldflags="-X ${package}.branch=${branch}" \
   ./...
-)
-
-if [[ "${TEST_PRESERVE_PIPELINES:-false}" == "true" ]]; then
-  echo Preserving pipelines for debugging
-  args+=(--preserve-pipelines)
-fi
-
-gotestsum "${args[@]}"
