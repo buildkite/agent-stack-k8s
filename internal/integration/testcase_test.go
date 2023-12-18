8a87d5a533d34caca0aa08d24ce63315659cccb5
Narthana Epa <narthana.epa@gmail.com>
2023-12-19 08:16:20 +1100

    Fix artifacts test helper
    It should work when there are not exactly 2 artifacts.

--- internal/integration/testcase_test.go
+++ internal/integration/testcase_test.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"crypto/sha256"
 	"fmt"
+	"slices"
 	"strconv"
 	"strings"
 	"testing"
@@ -190,10 +191,13 @@ func (t testcase) AssertArtifactsContain(build api.Build, expected ...string) {
 		nil,
 	)
 	require.NoError(t, err)
-	require.Len(t, artifacts, 2)
-	filenames := []string{*artifacts[0].Filename, *artifacts[1].Filename}
-	for _, filename := range expected {
-		assert.Contains(t, filenames, filename)
+
+	filenames := make([]string, 0, len(artifacts))
+	for _, filename := range artifacts {
+		filenames = append(filenames, *filename.Filename)
+	}
+	for _, e := range expected {
+		assert.True(t, slices.Contains(filenames, e), "expected %v to contain %v", filenames, e)
 	}
 }
 
