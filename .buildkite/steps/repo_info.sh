45b6ac709316ee459b2e707adfe39a5bcebb8c37
Josh Deprez <jd@buildkite.com>
2024-12-11 16:05:56 +1100

    Push images to Public ECR

--- .buildkite/steps/repo_info.sh
+++ .buildkite/steps/repo_info.sh
@@ -6,11 +6,30 @@ set -eufo pipefail
 tag="$(git describe)"
 version="${tag#v}"
 agent_image="$(buildkite-agent meta-data get agent-image)"
-controller_image="$(buildkite-agent meta-data get controller-image)"
-helm_repo="oci://ghcr.io/buildkite/helm"
-helm_image="${helm_repo}/agent-stack-k8s:${version}"
+
+# Is this a release version (version-tagged)?
+if [[ "${version}" == "${BUILDKITE_TAG#v}" ]] ; then
+    # Publish releases to both PECR and GHCR
+    controller_repo_pecr="public.ecr.aws/buildkite/agent-stack-k8s/controller"
+    controller_image_pecr="$(buildkite-agent meta-data get controller-image-pecr)"
+    helm_repo_pecr="oci://public.ecr.aws/buildkite/helm"
+    helm_image_pecr="${helm_repo_pecr}/agent-stack-k8s:${version}"
+
+    controller_repo_ghcr="ghcr.io/buildkite/agent-stack-k8s/controller"
+    controller_image_ghcr="$(buildkite-agent meta-data get controller-image-ghcr)"
+    helm_repo_ghcr="oci://ghcr.io/buildkite/helm"
+    helm_image_ghcr="${helm_repo_ghcr}/agent-stack-k8s:${version}"
+else
+    # Publish dev images to PECR dev repo only
+    controller_repo_pecr="public.ecr.aws/buildkite/agent-stack-k8s-dev/controller"
+    controller_image_pecr="$(buildkite-agent meta-data get controller-image-pecr)"
+    helm_repo_pecr="oci://public.ecr.aws/buildkite/helm-dev"
+    helm_image_pecr="${helm_repo_pecr}/agent-stack-k8s:${version}"
+fi
 
 echo "version=${version}"
-echo "controller_image=${controller_image}"
+echo "controller_image_pecr=${controller_image_pecr}"
+echo "controller_image_ghcr=${controller_image_ghcr:-}"
 echo "agent_image=${agent_image}"
-echo "helm_image=${helm_image}"
+echo "helm_image_pecr=${helm_image_pecr}"
+echo "helm_image_ghcr=${helm_image_ghcr:-}"
