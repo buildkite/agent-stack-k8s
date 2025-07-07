715038aed298d15e466469486467d316f8f2abd8
Ming <ming@buildkite.com>
2025-07-07 15:52:35 +1000

    PB-133: feature flag for job reservation support

--- internal/controller/config/config.go
+++ internal/controller/config/config.go
@@ -99,6 +99,9 @@ type Config struct {
 	// then the pod will malfunction.
 	AllowPodSpecPatchUnsafeCmdMod bool `json:"allow-pod-spec-patch-unsafe-command-modification" validate:"omitempty"`
 
+	// Enable job reservation support - this feature is in-progress.
+	ExperimentalJobReservationSupport bool `json:"experimental-job-reservation-support" validate:"omitempty"`
+
 	// These are only used for integration tests.
 	BuildkiteToken  string `json:"buildkite-token" validate:"omitempty"`
 	GraphQLEndpoint string `json:"graphql-endpoint" validate:"omitempty"`
@@ -138,6 +141,7 @@ func (c Config) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddUint16("prometheus-port", c.PrometheusPort)
 	enc.AddBool("prohibit-kubernetes-plugin", c.ProhibitKubernetesPlugin)
 	enc.AddBool("allow-pod-spec-patch-unsafe-command-modification", c.AllowPodSpecPatchUnsafeCmdMod)
+	enc.AddBool("experimental-job-reservation-support", c.ExperimentalJobReservationSupport)
 	if err := enc.AddArray("additional-redacted-vars", c.AdditionalRedactedVars); err != nil {
 		return err
 	}
