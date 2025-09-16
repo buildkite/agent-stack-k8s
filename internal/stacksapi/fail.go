57dec6577ba8c91d3d0d5de38daef6b4bbd4f47b
Ming <ming@buildkite.com>
2025-09-16 10:11:57 +1000

    PB-631 part 1: fail job API as part of stacks client

---
+++ internal/stacksapi/fail.go
@@ -0,0 +1,30 @@
+package stacksapi
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type FailJobRequest struct {
+	StackKey    string `json:"-"` // The key to call the stack. Required.
+	JobUUID     string `json:"-"` // The uuid of a job
+	ExitStatus  int    `json:"exit_status,omitzero"`
+	ErrorDetail string `json:"error_detail"` // An error detail string less than 4KB.
+}
+
+func (c *Client) FailJob(ctx context.Context, failJobReq FailJobRequest, opts ...RequestOption) error {
+	path := fmt.Sprintf("/stacks/%s/jobs/%s/fail", failJobReq.StackKey, failJobReq.JobUUID)
+	req, err := c.newRequest(ctx, http.MethodPost, path, failJobReq, opts...)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	resp, err := c.do(ctx, req, nil)
+	if err != nil {
+		return fmt.Errorf("fail job: %w", err)
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
