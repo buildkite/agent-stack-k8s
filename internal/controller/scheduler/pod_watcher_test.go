306b2b35b3177e421482658df516cb33a69a52e5
Jeremy Bumsted <jeremy@buildkite.com>
2026-03-17 10:09:06 -0600

    chore(tests): remove unused testify package

--- internal/controller/scheduler/pod_watcher_test.go
+++ internal/controller/scheduler/pod_watcher_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
